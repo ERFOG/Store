7e5eb967d7885e7cd9f60a108bcce7ad37b7db4d
ERFOG <140942004+ERFOG@users.noreply.github.com>
2023-11-28 19:34:25 +0700

    Add files via upload

---
+++ erfog/loginn.php
@@ -0,0 +1,118 @@
+<!doctype html>
+<html>
+  <title>Erfog Sore</title>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>wahyp.co.id</title>
+    <link rel="stylesheet" type="text/css" href="Assets/Css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="Assets/Css/style.css">
+  </head>
+  <body>
+    <script src="Assets/Js/bootstrap.bundle.min.js"></script>
+
+    <!--Awal navbar-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="#"><strong>ERFOG STORE</strong></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+
+          <form class="d-flex ms-auto my-4 my-lg-0 ">
+            <input class="form-control me-2" type="search" placeholder="Cari Barang Anda" aria-label="Search">
+            <button class="btn btn-light"type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+            </svg></button>
+          </form>
+
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link "  href="homepagewhyp.php">Beranda</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="kategoriwhyp.php">Kategori</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="produkwhyp.php">Produk</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://wa.link/ov1xzk">Kontak</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Login</a>
+            </li>
+
+            </li>
+          </ul>
+          <div class="d-grid gap-2 d-md-block me-2 ms-5 text-end">
+            <button class="btn btn-dark" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+              <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+              <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+            </svg>
+          </button>
+         </div>
+          </button>
+            
+          </div>
+        </div>
+      </div>
+    </nav>
+    <!-- akhir navbar-->
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <div class="col-md-4 offset-md-1">
+          <div class="card sticky center">
+            <div class="card-header bg-white text-center">
+           
+              <h3 class="text-judul">LOGIN </h3>
+
+            </div>
+<div class="login-register-area mt-no-text">
+        <div class="container custom-area">
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-custom">
+                    <div class="login-register-wrapper">
+                        <div class="section-content text-center mb-5">
+                            <p class="desc-content">Please login using account detail bellow.</p>
+                        </div>
+                        <form action="#" method="get">
+                            <div class="single-input-item mb-3">
+                                <input type="email" placeholder="Email or Username">
+                            </div>
+                            <div class="single-input-item mb-3">
+                                <input type="password" placeholder="Enter your Password">
+                            </div>
+                            <div class="single-input-item mb-3">
+                                <div class="login-reg-form-meta d-flex align-items-center justify-content-between">
+                                    <div class="remember-meta mb-3">
+                                        <div class="custom-control custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" id="rememberMe">
+                                            <label class="custom-control-label" for="rememberMe">Remember Me</label>
+                                        </div>
+                                    </div>
+                                    <a href="#" class="forget-pwd mb-3">Forget Password?</a>
+                                </div>
+                            </div>
+                            <div class="col">
+                  <a href="homepagewhyp.php"><button type="button" class="btn btn-success">Login</button></a>
+                </div>
+                            
+                         
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    <br><br><br> <br><br><br>
+    <footer>
+    <div class="footer">
+    <div class="copyright  md-4 py-3 text-center text-white bg-dark">
+      <div class="container"><medium>Copyright © <a href="https://instagram.com/erfogofficialstore" class="text-white" style="text-decoration: none;"> ERFOG STORE </a> 2023</medium\></div>
+  </div>
+ </div>
+  </footer>
+  </html>
